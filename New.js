8eb0a9abe7316554138dbfd2b49fcbe63fa71d50
acorrea9 <74523579+acorrea9@users.noreply.github.com>
2021-04-27 15:26:57 -0500

    Update New.js

--- New.js
+++ New.js
@@ -2,7 +2,11 @@ var ans = [];
 var values = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"];
 
 process.argv.slice(2).forEach((val, index) => {
-
+    var temp = "";
+    for(var x = 0; x < val.length; x++) {
+        temp += values[val.charAt(x) - '0'];
+    }
+    ans[index] = temp;
 })
 
 console.log(ans.toString());
