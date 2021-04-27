bb7c6cabada2c2e78bce25eda8dc754cab92a18a
acorrea9 <74523579+acorrea9@users.noreply.github.com>
2021-04-27 15:26:28 -0500

    Add files via upload

---
+++ New.js
@@ -0,0 +1,8 @@
+var ans = [];
+var values = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"];
+
+process.argv.slice(2).forEach((val, index) => {
+
+})
+
+console.log(ans.toString());
